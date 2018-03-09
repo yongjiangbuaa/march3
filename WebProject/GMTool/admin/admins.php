df838e368e576bc9c0f337efbafccf9b42157298
gengyongjiang <yongjiangbuaa@gmail.com>
2018-03-09 16:12:51 +0800

    modify log

--- WebProject/GMTool/admin/admins.php
+++ WebProject/GMTool/admin/admins.php
@@ -234,7 +234,7 @@ function get_ad_connection(){
 }
 
 function query_from_db($dbInfo,$sql) {
-    file_put_contents('/tmp/loginhis.log', "dbinfo=$dbInfo \n sql=".$sql."\n",FILE_APPEND);
+    file_put_contents('/tmp/loginhis.log', "cobar query db : \n sql=".$sql."\n",FILE_APPEND);
 
 
     
