fb2f046630be522cc3a980749069c9054ba95233
gengyongjiang <yongjiangbuaa@gmail.com>
2018-04-02 18:26:52 +0800

    去掉log

--- WebProject/GMTool/admin/control/server/server_log.php
+++ WebProject/GMTool/admin/control/server/server_log.php
@@ -4,7 +4,6 @@ if ($_REQUEST ['uid'])
 if( isset($uid)){
     $filename = '/usr/local/march/server.log';  //about 500MB
     $output = shell_exec('exec grep '.$uid .' ' . $filename . '| tail -n100 ');
-    file_put_contents('/tmp/loginhis.log', 'uid='.$uid.' log output ='.$output."\n",FILE_APPEND);
     echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
     exit;
 }else
