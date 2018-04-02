d1fd47b5696b01c42b9f34310bba5e5281879c4a
gengyongjiang <yongjiangbuaa@gmail.com>
2018-04-02 17:46:35 +0800

    修改查log工具

--- WebProject/GMTool/admin/control/server/server_log.php
+++ WebProject/GMTool/admin/control/server/server_log.php
@@ -1,3 +1,11 @@
 <?php
+if ($_REQUEST ['uid'])
+    $uid = $_REQUEST ['uid'];
+if( isset($uid)){
+    $filename = '/usr/local/march/server.log';  //about 500MB
+    $output = shell_exec('grep '.$uid .' ' . $filename . '| tail -n100 ');
+    echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
+    exit;
+}else
 include( renderTemplate("{$module}/{$module}_{$action}") );
 ?>
--- WebProject/GMTool/admin/tailLog.php
+++ WebProject/GMTool/admin/tailLog.php
@@ -1,5 +1,6 @@
 <?php
+$uid =
 $filename = '/usr/local/march/server.log';  //about 500MB
-$output = shell_exec('exec tail -n200 ' . $filename);  //only print last 50 lines
+$output = shell_exec('exec tail -n200 ' . $filename . '  grep '.$uid);  //only print last 50 lines
 echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
 ?>
