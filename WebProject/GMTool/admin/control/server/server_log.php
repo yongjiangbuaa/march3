b93e03b1db94fa5a1d2dd9407dc2aa9f81c56a3c
gengyongjiang <yongjiangbuaa@gmail.com>
2018-04-02 18:20:20 +0800

    加log

--- WebProject/GMTool/admin/control/server/server_log.php
+++ WebProject/GMTool/admin/control/server/server_log.php
@@ -3,7 +3,8 @@ if ($_REQUEST ['uid'])
     $uid = $_REQUEST ['uid'];
 if( isset($uid)){
     $filename = '/usr/local/march/server.log';  //about 500MB
-    $output = shell_exec('grep '.$uid .' ' . $filename . '| tail -n100 ');
+    $output = shell_exec('exec grep '.$uid .' ' . $filename . '| tail -n100 ');
+    file_put_contents('/tmp/loginhis.log', 'log output ='.$output."\n",FILE_APPEND);
     echo str_replace(PHP_EOL, '<br />', $output);         //add newlines
     exit;
 }else
